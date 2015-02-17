b60dd5ee4b7e983ce3952f435e42bec2c06e0698
Xavier Denis <xldenis@gmail.com>
2015-02-16 23:58:01 -0500

    Add pdf

---
+++ programs/test2.go
@@ -0,0 +1,18 @@
+package main
+//Test
+type BreakStuff struct {
+  func()
+  a MYOTHERTYPE
+  e int
+  a, b struct{} 
+}
+
+//Fib
+func main() {
+  x1 := 1
+  x2 := 1
+  for {
+    x1, x2 = x2, x1+x2
+    println(x1)
+  }
+}
--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,9 @@
 import System.Environment
 import MGC.Parser
 import Text.Parsec
+import System.FilePath.Posix
 
+import Control.Monad
 import MGC.Syntax.Pretty
 
 import Control.Applicative ((<$>), (<*))
@@ -9,8 +11,10 @@ import Control.Applicative ((<$>), (<*))
 main :: IO ()
 main = do
   args <- getArgs
-  ast  <- (parse (package <* eof) "") <$> readFile (head args)
+  let fname = head args
+  ast  <- (parse (package <* eof) "") <$> readFile (fname)
   case ast of 
     Left a -> putStrLn $ show a
     Right ast -> do 
       putStrLn $ prettyShow $ pPrint ast
+      writeFile (replaceExtension (fname) "pretty.go") $ prettyShow $ pPrint ast
